f1e7e21d722e7f8e30ed9f6d44b7465a1ad6347f
Jiang Yio <inportb@gmail.com>
2012-01-16 22:50:04 -0500

    fix python _ssl

--- cookbook/python2.7/recipe.mk
+++ cookbook/python2.7/recipe.mk
@@ -12,8 +12,8 @@ EXPORT_CLOBBER	+= $~/clobber
 
 # locals
 
-__CFLAGS	:= ${CFLAGS} -I${TOP}/$~/source/jni/libcrypt -I${TOP}/$~/source/jni/sqlite3
-__LDFLAGS	:= ${LDFLAGS} -L${TOP}/$~ -L${TOP}/$~/source
+__CFLAGS	:= ${CFLAGS} -I${TOP_INSTALL}/system/include/ncurses
+__LDFLAGS	:= ${LDFLAGS} -L${TOP}/$~/source
 __HPYTHON	:= ${TOP}/$~/build-host/bin/python
 __HPGEN		:= ${TOP}/$~/build-host/bin/pgen
 
@@ -40,9 +40,8 @@ $~/${ARCHIVE}:
 	wget http://www.python.org/ftp/python/${VERSION}/${ARCHIVE} -O $$@
 
 # JNI
-
-$~/libs: | $~/jni
-	cd $${@D}; ndk-build
+#$~/libs: | $~/jni
+#	cd $${@D}; ndk-build
 
 # host tools
 
@@ -55,20 +54,20 @@ $~/source-host/Makefile: | $~/${ARCHIVE}
 	touch $$@
 
 $~/build-host/.d: $~/source-host/Makefile
-	${MAKE} -C $~/source-host
+	${MAKE} -C $~/source-host python Parser/pgen
 	${MAKE} -C $~/source-host install
 	cp -lf $~/source-host/Parser/pgen $~/build-host/bin/
-	cd $${@D}; \
-		curl http://python-distribute.org/distribute_setup.py | ${__HPYTHON}; \
-		curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | ${__HPYTHON}; \
-		./bin/pip install virtualenv; \
-		./bin/pip install virtualenvwrapper
+	#cd $${@D}; \
+	#	curl http://python-distribute.org/distribute_setup.py | ${__HPYTHON}; \
+	#	curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | ${__HPYTHON}; \
+	#	./bin/pip install virtualenv; \
+	#	./bin/pip install virtualenvwrapper
 	touch $$@
 
 # target
 
 # depend on source-host to prevent race condition
-$~/source/Makefile: | $~/${ARCHIVE} $~/source-host/Makefile ${DIR_COOKBOOK}/readline/install ${DIR_COOKBOOK}/bzip2/install ${DIR_COOKBOOK}/openssl/install
+$~/source/Makefile: | $~/${ARCHIVE} $~/source-host/Makefile ${DIR_COOKBOOK}/readline/install ${DIR_COOKBOOK}/sqlite3/install ${DIR_COOKBOOK}/bzip2/install ${DIR_COOKBOOK}/openssl/install
 	if [ ! -d $${@D} ]; then \
 		tar zxf $~/${ARCHIVE} -C $~/; \
 		mv $~/${NAME}-${VERSION} $${@D}; \
@@ -77,7 +76,7 @@ $~/source/Makefile: | $~/${ARCHIVE} $~/source-host/Makefile ${DIR_COOKBOOK}/read
 			patch -p1 < ../patch/Python-2.7.2-android.patch; \
 			patch -p0 < ../patch/Python-2.7.2-regen.patch; \
 	fi
-	cd $${@D}; CC="agcc.bash" CFLAGS="${__CFLAGS}" LD="agcc.bash" LDFLAGS="${__LDFLAGS} -ldl" ./configure --host=arm-eabi --enable-shared \
+	cd $${@D}; CC="agcc.bash ${__CFLAGS}" LD="agcc.bash ${__CFLAGS}" LDFLAGS="${__LDFLAGS}" AR="${AR}" RANLIB="${RANLIB}" ./configure arm-linux --host=arm-eabi --enable-shared \
 		--prefix=/system \
 		--sbindir=/system/xbin \
 		--sharedstatedir=/data/local/com \
@@ -92,10 +91,10 @@ $~/source/Makefile: | $~/${ARCHIVE} $~/source-host/Makefile ${DIR_COOKBOOK}/read
 	mv temp $${@D}/pyconfig.h
 	touch $$@
 
-$~/build/.d: $~/source/Makefile | $~/libs $~/build-host/.d
-	${MAKE} -C $~/source BLDSHARED="agcc.bash -shared ${__CFLAGS} ${__LDFLAGS} -ldl -lbz2 -lreadline -lncurses" HOSTPYTHON="${__HPYTHON}" HOSTPGEN="${__HPGEN}" CROSS_COMPILE=arm-eabi CROSS_COMPILE_TARGET=yes HOSTARCH=arm-linux INSTSONAME=libpython2.7.so libpython2.7.so
-	ARCH="armeabi" NDKPLATFORM="${NDKPLATFORM}" ${MAKE} -C $~/source BLDSHARED="agcc.bash -shared ${__CFLAGS} ${__LDFLAGS} -ldl -lpython2.7" HOSTPYTHON="${__HPYTHON}" HOSTPGEN="${__HPGEN}" CROSS_COMPILE=arm-eabi CROSS_COMPILE_TARGET=yes HOSTARCH=arm-linux INSTSONAME=libpython2.7.so
-	ARCH="armeabi" NDKPLATFORM="${NDKPLATFORM}" ${MAKE} -C $~/source install HOSTPYTHON="${__HPYTHON}" CROSS_COMPILE=arm-eabi CROSS_COMPILE_TARGET=yes DESTDIR=${TOP}/$~/build
+$~/build/.d: $~/source/Makefile | $~/build-host/.d
+	ARCH="armeabi" NDKPLATFORM="${NDKPLATFORM}" ${MAKE} -C $~/source LDFLAGS="${__LDFLAGS} -ldl -lncurses -lreadline -lcrypto -lssl" HOSTPYTHON="${__HPYTHON}" HOSTPGEN="${__HPGEN}" CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so libpython2.7.so
+	ARCH="armeabi" NDKPLATFORM="${NDKPLATFORM}" ${MAKE} -C $~/source LDFLAGS="${__LDFLAGS} -ldl -lpython2.7" HOSTPYTHON="${__HPYTHON}" HOSTPGEN="${__HPGEN}" CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so
+	ARCH="armeabi" NDKPLATFORM="${NDKPLATFORM}" ${MAKE} -C $~/source install HOSTPYTHON="${__HPYTHON}" CROSS_COMPILE_TARGET=yes DESTDIR=${TOP}/$~/build
 	touch $$@
 
 endef
