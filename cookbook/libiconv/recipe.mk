76ed42f18f051c928344efbbeb9828e6b66315ee
Jiang Yio <inportb@gmail.com>
2012-01-22 04:07:05 -0500

    gnupg

---
+++ cookbook/libiconv/recipe.mk
@@ -0,0 +1,53 @@
+NAME	:= libiconv
+VERSION	:= 1.14
+ARCHIVE	:= ${NAME}-${VERSION}.tar.gz
+
+# exports
+
+EXPORT_MAKE		+= $~/install $~/package/*.yml
+EXPORT_INSTALL	+= $~/install
+EXPORT_PACKAGE	+= $~/package/*.yml
+EXPORT_CLEAN	+= $~/clean
+EXPORT_CLOBBER	+= $~/clobber
+
+define RECIPE
+
+# common targets
+
+.PHONY: $~/install $~/package/*.yml $~/clean $~/clobber
+$~/install: $~/build/.d
+	cp -rlf $~/build/* ${TOP_INSTALL}/
+$~/package/*.yml: $~/build/.d
+	cd ${DIR_REPO}; cat ${TOP}/$$@ | opkg-buildyaml ${TOP}/$~/build
+$~/clean:
+	${MAKE} -C $~/source clean
+$~/clobber:
+	rm -rf $~/source $~/build
+
+# build
+
+$~/${ARCHIVE}:
+	rm -rf $$@
+	wget http://ftp.gnu.org/pub/gnu/${NAME}/${ARCHIVE} -O $$@
+
+$~/source/Makefile: | $~/${ARCHIVE}
+	if [ ! -d $${@D} ]; then \
+		tar zxf $~/${ARCHIVE} -C $~/; \
+		mv $~/${NAME}-${VERSION} $${@D}; \
+	fi
+	cd $${@D}; CC="agcc.bash" CFLAGS="${CFLAGS}" LD="agcc.bash" LDFLAGS="${LDFLAGS}" STRIP="${STRIP} --strip-unneeded" ./configure --host=arm-android-eabi \
+		--prefix=/system \
+		--sbindir=/system/xbin \
+		--sharedstatedir=/data/local/com \
+		--localstatedir=/data/local/var \
+		--oldincludedir=/system/include
+	touch $$@
+
+$~/build/.d: $~/source/Makefile
+	${MAKE} -C $~/source
+	${MAKE} -C $~/source install DESTDIR=${TOP}/$~/build
+	touch $$@
+
+endef
+
+$(eval ${RECIPE})
---
+++ cookbook/libksba/recipe.mk
@@ -0,0 +1,54 @@
+NAME	:= libksba
+VERSION	:= 1.2.0
+ARCHIVE	:= ${NAME}-${VERSION}.tar.bz2
+
+# exports
+
+EXPORT_MAKE		+= $~/install $~/package/*.yml
+EXPORT_INSTALL	+= $~/install
+EXPORT_PACKAGE	+= $~/package/*.yml
+EXPORT_CLEAN	+= $~/clean
+EXPORT_CLOBBER	+= $~/clobber
+
+define RECIPE
+
+# common targets
+
+.PHONY: $~/install $~/package/*.yml $~/clean $~/clobber
+$~/install: $~/build/.d
+	cp -rlf $~/build/* ${TOP_INSTALL}/
+$~/package/*.yml: $~/build/.d
+	cd ${DIR_REPO}; cat ${TOP}/$$@ | opkg-buildyaml ${TOP}/$~/build
+$~/clean:
+	${MAKE} -C $~/source clean
+$~/clobber:
+	rm -rf $~/source $~/build
+
+# build
+
+$~/${ARCHIVE}:
+	rm -rf $$@
+	wget ftp://ftp.gnupg.org/gcrypt/${NAME}/${ARCHIVE} -O $$@
+
+$~/source/Makefile: | $~/${ARCHIVE} $(call COOK,libgpg-error)
+	if [ ! -d $${@D} ]; then \
+		tar jxf $~/${ARCHIVE} -C $~/; \
+		mv $~/${NAME}-${VERSION} $${@D}; \
+	fi
+	cd $${@D}; CC="agcc.bash" CFLAGS="${CFLAGS}" LD="agcc.bash" LDFLAGS="${LDFLAGS}" STRIP="${STRIP} --strip-unneeded" ./configure --host=arm-linux-androideabi \
+		--with-gpg-error-prefix=${TOP_INSTALL}/system \
+		--prefix=/system \
+		--sbindir=/system/xbin \
+		--sharedstatedir=/data/local/com \
+		--localstatedir=/data/local/var \
+		--oldincludedir=/system/include
+	touch $$@
+
+$~/build/.d: $~/source/Makefile
+	${MAKE} -C $~/source
+	${MAKE} -C $~/source install DESTDIR=${TOP}/$~/build
+	touch $$@
+
+endef
+
+$(eval ${RECIPE})
