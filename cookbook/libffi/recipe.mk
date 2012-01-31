45d264b36a3fabeac4c69440e1f86d7aadaf2360
Jiang Yio <inportb@gmail.com>
2012-01-30 22:18:58 -0500

    libffi

---
+++ cookbook/libffi/recipe.mk
@@ -0,0 +1,56 @@
+NAME	:= libffi
+VERSION	:= 3.0.10
+ARCHIVE	:= ${NAME}-${VERSION}.tar.gz
+
+# exports
+
+EXPORT_MAKE		+= $~/install $~/package/*.yml
+EXPORT_INSTALL	+= $~/install
+EXPORT_PACKAGE	+= $~/package/*.yml
+EXPORT_CLEAN	+= $~/clean
+EXPORT_CLOBBER	+= $~/clobber
+
+define RECIPE
+
+# common targets
+
+.PHONY: $~/install $~/package/*.yml $~/clean $~/clobber
+$~/install: $~/build/.d
+	cp -rlf $~/build/* ${TOP_INSTALL}/
+$~/package/*.yml: $~/build/.d
+	cd ${DIR_REPO}; cat ${TOP}/$$@ | opkg-buildyaml ${TOP}/$~/build
+$~/clean:
+	${MAKE} -C $~/source clean
+$~/clobber:
+	rm -rf $~/source $~/build
+
+# build
+
+$~/${ARCHIVE}:
+	rm -rf $$@
+	wget ftp://sourceware.org/pub/${NAME}/${ARCHIVE} -O $$@
+
+$~/source/configure: | $~/${ARCHIVE}
+	if [ ! -d $${@D} ]; then \
+		tar zxf $~/${ARCHIVE} -C $~/; \
+		mv $~/${NAME}-${VERSION} $${@D}; \
+	fi
+
+$~/source/Makefile: $~/source/configure
+	cd $${@D}; CC="agcc.bash" LD="agcc.bash" AR="${AR}" STRIP="${STRIP} --strip-unneeded" ./configure --host=arm-android-eabi \
+		--prefix=/system \
+		--sbindir=/system/xbin \
+		--sharedstatedir=/data/local/com \
+		--localstatedir=/data/local/var \
+		--oldincludedir=/system/include
+
+$~/build/.d: $~/source/Makefile
+	${MAKE} -C $~/source
+	${MAKE} -C $~/source install DESTDIR=${TOP}/$~/build
+	mv $${@D}/system/lib/${NAME}-${VERSION}/include $${@D}/system/
+	rm -rf $${@D}/system/lib/${NAME}-${VERSION} $${@D}/system/lib/pkgconfig
+	touch $$@
+
+endef
+
+$(eval ${RECIPE})
