73ab6c97677aea42c6ce723e9b55c4ee6740c140
Jiang Yio <inportb@gmail.com>
2012-01-31 17:01:36 -0500

    nettle+gmp=hogweed

--- cookbook/nettle/recipe.mk
+++ cookbook/nettle/recipe.mk
@@ -30,20 +30,23 @@ $~/${ARCHIVE}:
 	rm -rf $$@
 	wget http://www.lysator.liu.se/~nisse/archive/${ARCHIVE} -O $$@
 
-$~/source/configure: | $~/${ARCHIVE}
+$~/source/configure: | $~/${ARCHIVE}  ${DIR_COOKBOOK}/gmp/install
 	if [ ! -d $${@D} ]; then \
 		tar zxf $~/${ARCHIVE} -C $~/; \
 		mv $~/${NAME}-${VERSION} $${@D}; \
 	fi
 
 $~/source/Makefile: $~/source/configure
-	cd $${@D}; CC="agcc.bash" CXX="agcc-bash-g++" CXXFLAGS="-I${NDKPATH}/sources/cxx-stl/stlport/stlport -Duint64_t='unsigned long long'" LD="agcc.bash" LIBS="${NDKPATH}/sources/cxx-stl/stlport/libs/armeabi/libstlport_static.a" AR="${AR}" STRIP="${STRIP} --strip-unneeded" ./configure --host=arm-android-eabi --enable-shared \
+	cd $${@D}; CC="agcc.bash" CFLAGS="${CFLAGS}" CXX="agcc-bash-g++" CXXFLAGS="-I${NDKPATH}/sources/cxx-stl/stlport/stlport -Duint64_t='unsigned long long'" LD="agcc.bash" LDFLAGS="${LDFLAGS}" LIBS="${NDKPATH}/sources/cxx-stl/stlport/libs/armeabi/libstlport_static.a" AR="${AR}" STRIP="${STRIP} --strip-unneeded" ./configure --host=arm-android-eabi --enable-shared \
 		--prefix=/system \
 		--sbindir=/system/xbin \
 		--sharedstatedir=/data/local/com \
 		--localstatedir=/data/local/var \
 		--oldincludedir=/system/include
-	sed -E 's/^LIBNETTLE_(SONAME|FILE) = .*/LIBNETTLE_\1 = libnettle.so/g' $${@D}/config.make > temp
+	cat $${@D}/config.make \
+		| sed -E 's/^LIBNETTLE_(SONAME|FILE) = .*/LIBNETTLE_\1 = libnettle.so/g' \
+		| sed -E 's/^LIBHOGWEED_(SONAME|FILE) = .*/LIBHOGWEED_\1 = libhogweed.so/g' \
+		> temp
 	mv temp $${@D}/config.make
 	cat $${@D}/Makefile \
 		| sed -e 's/ln -sf \S* \S*/true/g' \
